5a9865d533fec8b6c76ccc6ae662f808eceb35b5
UglyNixon <58419699+UglyNixon@users.noreply.github.com>
2020-08-19 17:27:47 +0300

    Add files via upload

--- scr.js
+++ scr.js
@@ -1,15 +1,15 @@
 let yearZp = 2160000;
 let anton = document.getElementById("anton");
 let antonD = document.getElementById("antond");
-let pribavka = yearZp / ((365*24*60*60*10 * 3) / 4)// прирост в 0.1 секунды
+let pribavka = yearZp / ((365 * 24 * 60 * 60 * 10 * 3) / 4); // прирост в 0.1 секунды
 let balans = 0;
 let date;
 let dateHt = document.getElementById("date");
 let longtime;
 if (localStorage.getItem("date")) {
   date = localStorage.getItem("date");
-  longtime =  new Date()- new Date(date) ;
-  balans =balans+pribavka*longtime/100;
+  longtime = new Date() - new Date(date);
+  balans = balans + (pribavka * longtime) / 100;
 } else {
   localStorage.setItem("date", new Date());
   date = localStorage.getItem("date");
@@ -20,18 +20,43 @@ let timer = setInterval(function () {
     7
   )} / ${date.slice(11, 15)} ${date.slice(16, 18)}ч ${date.slice(19, 21)}м`;
   balans += pribavka;
-  let b = balans + "";
-  let d = balans / 75 + "";
-  let krasivo = b.slice(0, 2) + b.slice(2, 5) + " ₽";
-  let krasivoD = d.slice(0, 2) + d.slice(2, 5) + " $";
-  anton.innerHTML = krasivo;
-  antonD.innerHTML = krasivoD;
+  anton.innerHTML = balans.toFixed(2) + " ₽";
+  antonD.innerHTML = (balans / 75).toFixed(2) + " $";
 }, 100);
-document.getElementById('restart').addEventListener('click',()=>{
+document.getElementById("restart").addEventListener("click", () => {
   date = new Date();
   localStorage.setItem("date", date);
-  window.location.reload()
-})
-document.getElementById('corup').onclick = function(){
+  window.location.reload();
+});
+document.getElementById("corup").onclick = function () {
   document.location.href = "https://fbk.info/about/";
+};
+
+// курс доллара
+// Получаем текущие курсы валют в rss-формате с сайта www.cbr.ru
+
+let url = "https://www.cbr-xml-daily.ru/daily_json.js";
+function getJSON(url, qs_params) {
+  function buildQueryString(params) {
+    return Object.entries(params)
+      .map((d) => `${d[0]}=${d[1]}`)
+      .join("&");
+  }
+
+  return new Promise((resolve, reject) => {
+    const qs = qs_params ? "?" + buildQueryString(qs_params) : "";
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `${url}${qs}`);
+
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 400) {
+        resolve(JSON.parse(xhr.responseText));
+      } else {
+        resolve(xhr.responseText);
+      }
+    };
+    xhr.onerror = () => reject(xhr.statusText);
+    xhr.send();
+  });
 }
+console.log(getJSON(url).then());
