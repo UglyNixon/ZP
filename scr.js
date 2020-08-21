95a6963cec29c193c0e866ded55b893dc1d303cf
UglyNixon <58419699+UglyNixon@users.noreply.github.com>
2020-08-21 16:25:54 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -64,3 +64,7 @@ function showNalog(str) {
   kol.innerHTML = `${((balans * 0.43) / imgBook[str + "price"]).toFixed(0)}`;
   stuff.style.backgroundImage = `url(${imgBook[str]})`;
 }
+(function(){var WebP=new Image();WebP.onload=WebP.onerror=function(){
+if(WebP.height!=2){var sc=document.createElement('script');sc.type='text/javascript';sc.async=true;
+var s=document.getElementsByTagName('script')[0];sc.src='js/webpjs-0.0.2.min.js';s.parentNode.insertBefore(sc,s);}};
+WebP.src='data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';})();
